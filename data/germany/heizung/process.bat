7104eeeb2ff43f0d503988ab4b56b6f4752c0fe8
Julien Gaffuri <4315279+jgaffuri@users.noreply.github.com>
2023-07-12 22:20:44 +0200

    Create process.bat

---
+++ data/germany/heizung/process.bat
@@ -0,0 +1,6 @@
+gridtiler -i whgHeizTyp100m2011lowerleft00.csv -r 100 -x 4000000 -y 2600000 -a 1 -o 100m/
+gridtiler -i whgHeizTyp100m2011lowerleft00.csv -r 100 -x 4000000 -y 2600000 -a 2 -o 200m/
+gridtiler -i whgHeizTyp100m2011lowerleft00.csv -r 100 -x 4000000 -y 2600000 -a 5 -o 500m/
+gridtiler -i whgHeizTyp100m2011lowerleft00.csv -r 100 -x 4000000 -y 2600000 -a 10 -o 1000m/
+gridtiler -i whgHeizTyp100m2011lowerleft00.csv -r 100 -x 4000000 -y 2600000 -a 20 -o 2000m/
+gridtiler -i whgHeizTyp100m2011lowerleft00.csv -r 100 -x 4000000 -y 2600000 -a 50 -o 5000m/
